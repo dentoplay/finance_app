1292d0133849993cf0e51291f939878118d70dad
Alexander <10alexgerman10@gmail.com>
2026-03-03 20:09:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = "finance-zone-v1";
+const CACHE_NAME = "finance-cache-v1";
+
 const urlsToCache = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
   "/manifest.json",
-  "/icon.png"
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
